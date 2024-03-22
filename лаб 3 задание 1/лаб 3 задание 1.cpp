582639710ab5ab009ba8c232ca0a1174574b469d
Gregory <Gregory@ZENBOOK>
2024-03-22 16:32:56 +0300

    Добавьте файлы проекта.

---
+++ лаб 3 задание 1/лаб 3 задание 1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+class Time {
+private:
+	int minute = 0;
+	int second = 0;
+public:
+	void get() {
+		cin >> minute;
+		cin >> second;
+	}
+	void print() {
+		int minute1 = 60 * minute;
+		cout << "общее количество секунд " << minute1 + second;
+	}
+
+
+};
+int main() {
+	Time time;
+	setlocale(LC_ALL, "RU");
+	cout << "Введите количество минут и секунд" << endl;
+	time.get();
+	time.print();
+}
